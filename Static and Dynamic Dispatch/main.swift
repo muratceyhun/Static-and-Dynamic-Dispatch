1268ae4225ab047a73d5389a80b71edf56c7a16b
Murat Ceyhun KORPEOGLU <muratceyhunkorpeoglu@gmail.com>
2023-03-20 23:29:57 +0300

    updates

--- Static and Dynamic Dispatch/main.swift
+++ Static and Dynamic Dispatch/main.swift
@@ -55,8 +55,6 @@ circle.area()
 
 
 
-
-
 /*Method Dispatch
 Method dispatching is when a program is executing and faces a method call. The program needs to dispatch to the address of this method’s implementation. Sometimes we expect that the implementation for a method is determined only at runtime.
 
